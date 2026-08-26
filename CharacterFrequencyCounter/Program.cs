537271ea0cdd4dd1c74d1f62f74bbf018edd777b
1nayat <inayatw086@gmail.com>
2026-08-26 13:30:29 +0530

    added chr freq counter

---
+++ CharacterFrequencyCounter/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string text = "inayat";
+        int[] freq = CountLetters(text);
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] > 0)
+            {
+                char letter = (char)('a' + i);
+                Console.WriteLine($"Letter {letter} : {freq[i]} times ");
+            }
+        }
+    }
+
+    public static int[] CountLetters(string s )
+    {
+        int[] count = new int[26];
+        foreach(char c in s)
+        {
+            count[c - 'a']++;
+        }
+        return count;
+    }
+}
--- RangeUpdateUsingDifferenceArray/Program.cs
+++ RangeUpdateUsingDifferenceArray/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    static void Main()
+    {
+        int n = 6;
+        int[][] updates = new int[][]
+        {
+            new int [] {1,3,6}
+        };
+        int[] result = ApplyRangeUpdates(n, updates);
+        Console.WriteLine(string.Join(" ", result));
+    }
+    public static int[] ApplyRangeUpdates(int n , int[][] updates)
+    {
+        int[] diff = new int[n + 1];
+
+        foreach (int[] u in updates)
+        {
+            int l = u[0];
+            int r = u[1];
+            int v = u[2];
+
+            diff[l]  += v;
+            diff[r + 1] -= v;
+        }
+        int[] result = new int[n];
+        int running = 0;
+        for (int i =0;i<n;i++)
+        {
+            running += diff[i];
+            result[i] = running;
+        }
+        return result;
+    }
+}
