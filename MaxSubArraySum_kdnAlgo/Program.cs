1577581f29845ff5948171ddd272db649192acc1
1nayat <inayatw086@gmail.com>
2026-08-28 23:13:29 +0530

    added kadane

---
+++ MaxSubArraySum_kdnAlgo/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] arr = { -2, -8, 1, 9, -2, 3 };
+        int result = MaxSubArray(arr);
+        Console.WriteLine(result);
+    }
+    public static int MaxSubArray(int[] arr)
+    {
+        int best = arr[0];
+        int current = arr[0];
+
+        for (int i =0; i<arr.Length;i++)
+        {
+            current = Math.Max(arr[i], current + arr[i]);
+            best = Math.Max(best, current);
+        }
+        return best;
+    }
+
+}
