20a4440d1338db0e9348128ffccf379ea0b507a5
1nayat <inayatw086@gmail.com>
2026-08-29 22:36:31 +0530

    added maxprod

---
+++ MaxProductSubArray/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] arr = { -2, 3, -2, 4 };
+        int result = MaxProduct(arr);
+        Console.WriteLine(string.Join(" ", result));   
+    }
+    public static int MaxProduct(int[] arr)
+    {
+        int best = arr[0];
+        int mincur = arr[0];
+        int maxcur = arr[0];
+
+        for (int i =1; i<arr.Length;i++)
+        {
+            int x = arr[i];
+            if (x<0)
+            {
+                (maxcur, mincur) = (mincur, maxcur);
+
+            }
+            maxcur = Math.Max(x, maxcur * x);
+            mincur = Math.Min(x, mincur * x);
+            best = Math.Max(best, maxcur);
+        }
+        return best;
+    }
+
+}
