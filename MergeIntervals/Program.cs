de88ad710b5666882a97bf295a7a6ffbfa7e02dd
1nayat <inayatw086@gmail.com>
2026-09-01 17:28:41 +0530

    added merge intervals

---
+++ MergeIntervals/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[][] intervals =
+            {
+        new int[] {1,3},
+        new int[] { 2, 6 },
+        new int[] { 8, 10 },
+        new int[] { 15, 18 }
+        };
+        int[][] result = MergeIntervals(intervals);
+        foreach (int[] interval in result)
+        {
+            Console.WriteLine(interval[0] + "," + interval[1]);
+        }
+    }
+
+    public static int[][] MergeIntervals(int[][] intervals)
+    {
+        if (intervals.Length == 0)
+            return intervals;
+        Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
+        List<int[]> merged = new List<int[]>();
+        int[] current = intervals[0];
+
+        for(int i = 1; i < intervals.Length; i++)
+        {
+            if (intervals[i][0] <= current[1])
+            {
+                current[1] = Math.Max(current[1], intervals[i][1]);
+
+            }
+            else
+            {
+                merged.Add(current);
+                current = intervals[i];
+            }
+        }
+        merged.Add(current);
+        return merged.ToArray();
+    }
+}
