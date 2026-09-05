1f35b8c80c5ab85834105f64a71da2277b75a5ee
1nayat <inayatw086@gmail.com>
2026-09-06 01:08:32 +0530

    frequencyMap

---
+++ FrequencyMap/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string s = "Bbbanana546";
+        Dictionary<char, int> result = FrequencyMap(s);
+        foreach(var item in result)
+        {
+            Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+    public static Dictionary<char,int> FrequencyMap(string s)
+    {
+        var count = new Dictionary<char, int>();
+        
+        foreach(char c in s)
+        {
+            count.TryGetValue(c, out int exixting);
+            count[c] = exixting + 1;
+        }
+        return count;
+    }
+
+}
